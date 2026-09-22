7705016424d644db73effd65a2e0197cf84a12af
Lakshay <black.leopard0987@gmail.com>
2026-09-22 17:32:12 +0530

    NFC Users API

--- UserCred_API/controllers/nfcController.js
+++ UserCred_API/controllers/nfcController.js
@@ -5,10 +5,9 @@ const readNfcTag = async (req, res) => {
     if (!id) {
         res.status(400).json({ message: "missing required fields", data: null });
     }
-
     try {
         const pool = await poolPromise;
-        const response = await pool.request().input("id", id).query("SELECT * FROM user_credentials WHERE entryPass=@id");
+        const response = await pool.request().input("id", mssql.VarChar, id).query("SELECT * FROM user_credentials WHERE entryPass=@id");
         return res.status(200).json({ message: "Successfully fetched tag", data: response.recordSet[0] });
     } catch (error) {
         return res.status(500).json({ message: "Error reading nfc : " + error.message, data: null })
